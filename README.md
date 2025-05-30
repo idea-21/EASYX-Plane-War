17f88f691f944f478832e02ed782f151c721d707
idea-21 <g213063434@gmail.com>
2025-05-31 07:36:03 +0800

    add overlap check

---
+++ README.md
@@ -0,0 +1,2 @@
+**解决了生成重叠问题：**
+![218c11b20a8f260ae513078a3d75a2fe.png](./218c11b20a8f260ae513078a3d75a2fe.png)![demo.gif](./demo.gif)
--- graph/graph.cpp
+++ graph/graph.cpp
@@ -58,14 +58,14 @@ void initial()
 	initgraph(WIN_WIDTH, WIN_HEIGHT);
 
 	loadimage(&background, L"image\\bk.png", 480, 700);
-	loadimage(&img_ally[0], L"image\\me1.png", 102, 126, NOTSRCERASE);
-	loadimage(&img_ally[1], L"image\\me2.png", 102, 126, SRCINVERT);
-	loadimage(&img_enemy[0][0], L"image\\enemy2.png", 69, 99, NOTSRCERASE);
-	loadimage(&img_enemy[0][1], L"image\\enemy2_hit.png", 69, 99, SRCINVERT);
-	loadimage(&img_enemy[1][0], L"image\\enemy3_n1.png", 169, 258, NOTSRCERASE);
-	loadimage(&img_enemy[1][1], L"image\\enemy3_n2.png", 169, 258, SRCINVERT);
-	loadimage(&img_bullet[0], L"image\\bullet1.png", 5, 11, NOTSRCERASE);
-	loadimage(&img_bullet[1], L"image\\bullet2.png", 5, 11, SRCINVERT);
+	loadimage(&img_ally[0], L"image\\me1.jpg", 102, 126, NOTSRCERASE);
+	loadimage(&img_ally[1], L"image\\me2.jpg", 102, 126, SRCINVERT);
+	loadimage(&img_enemy[0][0], L"image\\enemyPlane1.jpg", 69, 99, NOTSRCERASE);
+	loadimage(&img_enemy[0][1], L"image\\enemyPlane2.jpg", 69, 99, SRCINVERT);
+	loadimage(&img_enemy[1][0], L"image\\enemyPlane1.jpg", 80, 110, NOTSRCERASE);
+	loadimage(&img_enemy[1][1], L"image\\enemyPlane2.jpg", 80, 110, SRCINVERT);
+	loadimage(&img_bullet[0], L"image\\bullet1.jpg", 5, 11, NOTSRCERASE);
+	loadimage(&img_bullet[1], L"image\\bullet2.jpg", 5, 11, SRCINVERT);
 }
 
 //put images
@@ -115,28 +115,73 @@ void createbullet()
 }
 
 //create enemies
-void createenemy()
-{
-	for (int i = 0; i < ENEMY_NUM; i++)
-	{
-		if (!enemy[i].live)
-		{
-			enemy[i].x = rand() % (WIN_WIDTH - 169);
-			enemy[i].y = 0;
+bool check_collision_rect(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2) {
+	return x1 < x2 + w2 &&
+		x1 + w1 > x2 &&
+		y1 < y2 + h2 &&
+		y1 + h1 > y2;
+}
+
+void createenemy() {
+	for (int i = 0; i < ENEMY_NUM; i++) {
+		if (!enemy[i].live) {
+			int new_x, new_y = 0;
+			int new_width, new_height;
+			int new_hp;
+			int new_type;
+
+			const int MAX_PLACEMENT_ATTEMPTS = 10;
+			bool position_found = false;
+
+			for (int attempt = 0; attempt < MAX_PLACEMENT_ATTEMPTS; ++attempt) {
+				new_type = rand() % 2 == 0 ? SMALL : BIG;
+				if (new_type == BIG) {
+					new_hp = 3;
+					new_width = 80;
+					new_height = 110;
+				}
+				else {
+					new_hp = 1;
+					new_width = 69;
+					new_height = 99;
+				}
+
+				// Generate a random x position
+				if (WIN_WIDTH - new_width <= 0) {
+					new_x = 0;
+				}
+				else {
+					new_x = rand() % (WIN_WIDTH - new_width + 1);
+				}
+
+
+				// Check for overlap with existing live enemies
+				bool overlaps = false;
+				for (int j = 0; j < ENEMY_NUM; ++j) {
+					if (i == j || !enemy[j].live) {
+						continue;
+					}
+					if (check_collision_rect(new_x, new_y, new_width, new_height,
+						enemy[j].x, enemy[j].y, enemy[j].width, enemy[j].height)) {
+						overlaps = true;
+						break; // Overlaps with an existing enemy
+					}
+				}
+
+				if (!overlaps) {
+					position_found = true;
+					break; // Found a good position
+				}
+			}
+
+			enemy[i].x = new_x;
+			enemy[i].y = new_y;
 			enemy[i].live = true;
-			enemy[i].type = rand() % 2 == 0 ? SMALL : BIG;
-			if (enemy[i].type == BIG)
-			{
-				enemy[i].hp = 3;
-				enemy[i].width = 169;
-				enemy[i].height = 258;
-			}
-			if (enemy[i].type == SMALL)
-			{
-				enemy[i].hp = 1;
-				enemy[i].width = 69;
-				enemy[i].height = 99;
-			}
+			enemy[i].type = new_type;
+			enemy[i].hp = new_hp;
+			enemy[i].width = new_width;
+			enemy[i].height = new_height;
+
 			break;
 		}
 	}
